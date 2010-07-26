792c417ef392241c8338881ce56b5b1b5b7930a9
joseluiz <joseluiz@joseluiz-desktop>
2010-07-26 14:33:26 -0300

    Retirada de Warning das Exceptions. (testando commit)

--- src/model/exception/ColisaoDectadaException.java
+++ src/model/exception/ColisaoDectadaException.java
@@ -1,5 +1,10 @@
 package model.exception;
 
 public class ColisaoDectadaException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4381803079237470568L;
 	
 }
--- src/model/exception/QuadroDescartadoException.java
+++ src/model/exception/QuadroDescartadoException.java
@@ -2,4 +2,9 @@ package model.exception;
 
 public class QuadroDescartadoException extends Exception{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 9175128995839682702L;
+
 }
